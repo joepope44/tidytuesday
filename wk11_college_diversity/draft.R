b53850e1131cb5524bd3bc8cdd9fcc27da5f05c8
josephpope <joepope44@gmail.com>
2020-03-10 23:20:42 -0400

    "initial setup script"

---
+++ wk11_college_diversity/draft.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+
+tuition_cost <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-10/tuition_cost.csv')
+
+tuition_income <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-10/tuition_income.csv') 
+
+salary_potential <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-10/salary_potential.csv')
+
+historical_tuition <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-10/historical_tuition.csv')
+
+diversity_school <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-10/diversity_school.csv')
+
+df <- dplyr::left_join(tuition_cost, diversity_school, 
+                   by = c("name", "state"))
