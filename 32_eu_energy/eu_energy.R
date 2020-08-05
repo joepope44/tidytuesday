2383b6115a840a9247a8f859461a5a825acdbd55
josephpope <joepope44@gmail.com>
2020-08-04 22:05:20 -0400

    added graph

--- 32_eu_energy/eu_energy.R
+++ 32_eu_energy/eu_energy.R
@@ -8,30 +8,36 @@ energy_types <- tuesdata$energy_types
 
 country_totals <- tuesdata$country_totals
 
+energy_totals <- energy_types %>%
+  select(-level) %>%
+  pivot_longer(cols = c(`2016`, `2017`, `2018`), names_to = "year", values_to = "total_power") %>%
+  group_by(country, year) %>%
+  summarise(total = sum(total_power)) %>%
+  ungroup()
 
-solar <- energy_types %>% 
-  filter(type == "Solar") %>% 
-  mutate(yoy_2017 = round((`2017` / `2016` - 1) * 100, 2),
-         yoy_2018 = round((`2018` / `2017` - 1) * 100, 2),
-         total = `2016` + `2017` + `2018`) %>% 
-  filter(total > 10 & `2018` > 0) %>% 
-  arrange(desc(total)) 
+solar_rates <- energy_types %>%
+  filter(type == "Solar") %>%
+  select(-type, -level) %>%
+  mutate(
+    country_name = case_when(country_name == "North Macedonia" ~ "N. Macedonia",
+                       country_name == "Bosnia & Herzegovina" ~ "Bosnia & H.",
+                       country == "UK" ~ "UK",
+                       TRUE ~ country_name)
+    ) %>%
+  pivot_longer(cols = c(`2016`, `2017`, `2018`), names_to = "year", values_to = "solar_power") %>%
+  left_join(energy_totals, by = c("country" = "country", "year" = "year")) %>%
+  mutate(solar_rate = round(solar_power / total * 100, 2))
 
-         
-solar_rates <- solar %>% 
-  select(country, country_name, yoy_2017, yoy_2018) %>% 
-  mutate(country_name = ifelse(country == "UK", "United Kingdom", country_name),
-         `2017` = yoy_2017, `2018` = yoy_2018) %>% 
-  select(-yoy_2017, -yoy_2018) %>% 
-  pivot_longer(names_to = "Year", values_to = "YoY Rate", cols = c(`2017`, `2018`))
-
-solar_rates2 <- energy_types %>% 
-  filter(type == "Solar" & `2018` > 0) %>% 
-  select(-type, -level) %>% 
-  mutate(country_name = ifelse(country == "UK", "United Kingdom", country_name)) %>% 
-  pivot_longer(cols = c(`2016`, `2017`, `2018`), names_to = "year", values_to = "solar_power")
-
-ggplot(solar_rates2, aes(year, solar_power)) + 
-  geom_col() + 
-  facet_geo(~ country_name, grid = "europe_countries_grid1")
- 
+ggplot(solar_rates, aes(year, solar_rate)) +
+  geom_col() +
+  facet_geo(~country_name, grid = europe_countries_grid2 %>% mutate(name = str_trunc(name, 11))) +
+  theme_solarized() +
+  scale_x_discrete(labels = c('`16','17','18')) + 
+  scale_y_continuous(breaks = c(0,8,16)) +
+  labs(
+    title = "Solar as % of Total Power Genereated in the EU by Year",
+    subtitle = "A Surprising Lack of Solar Power in Southern Europe/n",
+    caption = "Joseph Pope | @joepope44",
+    x = "",
+    y = ""
+  )
